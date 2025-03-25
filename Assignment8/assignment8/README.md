7e7a9b51711b9c41f2662aca259ebfffecee5f20
JoshuaT35 <joshuatablan03@gmail.com>
2025-03-25 10:20:20 -0600

    added assignment 8

---
+++ Assignment8/assignment8/README.md
@@ -0,0 +1,15 @@
+# ECE421 Assignment 7 Question 2 - AKA Assignment7_Q2
+
+## Author
+Name: Joshua Tablan\
+CCID: tablan\
+student ID: 1726177
+
+### To build
+- Run `cargo build`.
+
+### To run `main()`
+- Run `cargo run`.
+
+## NOTES
+Based on code given in ECE 421 Assignment 8,also found here [https://github.com/uofa-ece421/sync-server](Sync Server: Assignment 8 provided code).
---
+++ Assignment8/assignment8/src/main.rs
@@ -0,0 +1,66 @@
+use chrono::Utc;
+use std::io;
+use tokio::time::{sleep, Duration};
+
+use std::collections::HashMap;
+
+async fn handle_cmd(cmd: char) {
+    match cmd {
+        'i' => {
+            if let Ok(resp) = reqwest::get("https://httpbin.org/ip").await {
+                if let Ok(ip) = resp.json::<HashMap<String, String>>().await {
+                    println!("{:#?}", ip);
+                }
+            }
+        },
+        'd' => if let Ok(resp) = reqwest::get("https://httpbin.org/delay/9").await {
+                 println!("{:#?}", resp);
+        },
+        other => println!("unknown cmd: {}", other),
+    }
+}
+
+async fn read_cmd() -> Result<usize, Box<dyn std::error::Error>> {
+    let mut data = String::new();
+    let stdin = io::stdin();
+    match stdin.read_line(&mut data) {
+        Ok(len) => {
+            if len > 0 {
+                if let Some(cmd) = data.chars().next() {
+                    handle_cmd(cmd).await;
+                }
+            }
+            Ok(len)
+        },
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+async fn periodic_task(interval: u64) {
+    loop {
+        sleep(Duration::from_secs(interval)).await;
+        println!("UTC now: {}", Utc::now());
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let task_handle = tokio::spawn(periodic_task(3));
+
+    loop {
+        match read_cmd().await {
+            Ok(len) => {
+                if len == 0 {
+                    break;
+                }
+            }
+            Err(e) => println!("input error {}", e),
+        }
+    }
+
+    // main waits for spawned tasks to finish
+    let _ = tokio::join!(task_handle);
+
+    Ok(())
+}
